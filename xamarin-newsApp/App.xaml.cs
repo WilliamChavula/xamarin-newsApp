c2b35589d9c9507ccc9c9366a0845250c7e8b2e6
WilliamChavula <rumbani@gmail.com>
2021-02-09 21:50:17 -0600

    * .gitignore: initial commit. Configured Prism in the Xamarin.Forms   app and the Native apps.
    * xamarin-newsApp.sln:
    * App.xaml:
    * Main.cs:
    * App.xaml.cs:
    * MainPage.xaml:
    * Info.plist:
    * AssemblyInfo.cs:
    * MainPage.xaml.cs:
    * AppDelegate.cs:
    * Entitlements.plist:
    * xamarin-newsApp.csproj:
    * MainActivity.cs:
    * Chomsky.otf:
    * Default.png:
    * Default@2x.png:
    * xamarin-newsApp.iOS.csproj:
    * AssemblyInfo.cs:
    * OpenSans-Bold.ttf:
    * AboutAssets.txt:
    * OpenSans-Regular.ttf:
    * Default-568h@2x.png:
    * Default-Portrait.png:
    * OpenSans-SemiBold.ttf:
    * AssemblyInfo.cs:
    * colors.xml:
    * styles.xml:
    * Tabbar.xml:
    * AboutResources.txt:
    * Toolbar.xml:
    * Default-Portrait@2x.png:
    * LaunchScreen.storyboard:
    * Resource.designer.cs:
    * AndroidManifest.xml:
    * xamarin-newsApp.Android.csproj:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.png:
    * PlayfairDisplay-Regular.ttf:
    * icon.png:
    * PlayfairDisplay-SemiBold.ttf:
    * icon.xml:
    * Icon87.png:
    * Icon80.png:
    * Icon29.png:
    * Icon40.png:
    * Icon20.png:
    * Icon60.png:
    * Icon76.png:
    * Icon58.png:
    * Icon180.png:
    * Icon167.png:
    * Icon152.png:
    * Icon120.png:
    * icon_round.xml:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ xamarin-newsApp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace xamarin_newsApp
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer)
+        {
+
+        }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+            NavigationService.NavigateAsync("MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage>();
+        }
+    }
+}
---
+++ xamarin-newsApp/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Chomsky.otf", Alias = "Chomsky-Font")]
+[assembly: ExportFont("OpenSans-Bold.ttf", Alias = "OpenSans-Bold")]
+[assembly: ExportFont("OpenSans-Regular.ttf", Alias = "OpenSans-Regular")]
+[assembly: ExportFont("OpenSans-SemiBold.ttf", Alias = "OpenSans-SemiBold")]
+[assembly: ExportFont("PlayfairDisplay-Regular.ttf", Alias = "PlayfairDisplay-Regular")]
+[assembly: ExportFont("PlayfairDisplay-SemiBold.ttf", Alias = "PlayfairDisplay-SemiBold")]
